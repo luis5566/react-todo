8829d43fc197c0abde1ec8e2a5565c28e4eebee9
Luis Martins <lg.martins@campus.fct.unl.pt>
2017-04-02 21:34:07 +0100

    Actions and reducers

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -2,7 +2,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var TestUtils = require('react-addons-test-utils');
 var expect = require('expect');
-var $ = require('jquery');
 
 var Todo = require('Todo');
 
